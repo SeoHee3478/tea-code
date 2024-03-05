e3115d92753de1191e77b11d1d2515b092e6b856
minkyeongJ <jomk2221@naver.com>
2024-03-05 12:16:56 +0900

    Feat: 이미지 레이지 로딩 구현 코드

--- app/src/pages/ImageLazyLoading/LazyImage.jsx
+++ app/src/pages/ImageLazyLoading/LazyImage.jsx
@@ -1,7 +1,50 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
 
-const LazyImage = ({ src, alt, className }) => {
-  return <img className={className} alt={alt} id={src} src={src} />;
+const LazyImage = ({ src, alt, className, height }) => {
+  const imgRef = useRef(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    let observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            imgRef.current.src = src;
+            imgRef.current.onload = () => {
+              setIsLoaded(true);
+              observer.disconnect();
+            };
+          }
+        });
+      },
+      {
+        rootMargin: "100px",
+      }
+    );
+
+    if (imgRef?.current) {
+      observer.observe(imgRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [src]);
+
+  return isLoaded ? (
+    <img className={className} alt={alt} id={src} src={src} />
+  ) : (
+    <img
+      className={className}
+      ref={imgRef}
+      id={src}
+      style={{
+        height: `${height}px`,
+        backgroundColor: "#b9d79c",
+      }}
+    />
+  );
 };
 
 LazyImage.propTypes = {
