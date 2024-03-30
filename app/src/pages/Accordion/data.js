206512a012b5d83cc59985fbd46f68100a9ee582
wSeungMi <seungmi.dev@gmail.com>
2024-03-30 20:38:29 +0900

    Feat: FAQ 데이터 추가

--- app/src/pages/Accordion/data.js
+++ app/src/pages/Accordion/data.js
@@ -1,24 +1,28 @@
 const data = [
   {
-    id: 1,
+    id: "d-1",
+    number: 1,
     question: "What is JavaScript?",
     answer:
       "JavaScript is a programming language commonly used in web development to add interactivity and dynamic behavior to web pages. It allows developers to create client-side scripts that run within the browser.",
   },
   {
-    id: 2,
+    id: "d-2",
+    number: 2,
     question: "What is a Frontend Framework?",
     answer:
       "A frontend framework is a collection of pre-written, standardized code that developers can use to expedite the process of building user interfaces for web applications. Examples include React, Angular, and Vue.js.",
   },
   {
-    id: 3,
+    id: "d-3",
+    number: 3,
     question: "What is Test-Driven Development (TDD)?",
     answer:
       "Test-Driven Development (TDD) is a software development process that emphasizes writing automated tests before writing the code. Developers write failing tests first, then write the minimum amount of code necessary to pass the tests, and finally refactor the code to improve its design.",
   },
   {
-    id: 4,
+    id: "d-4",
+    number: 4,
     question: "What is DevOps?",
     answer:
       "DevOps is a set of practices that combines software development (Dev) and IT operations (Ops) to improve collaboration and automation throughout the software development lifecycle. It aims to shorten the system development life cycle and provide continuous delivery with high quality.",
