9618b94682fd101aebdafde7fbeb284f639b2a2a
minkyeongJ <jomk2221@naver.com>
2024-04-12 16:16:46 +0900

    Feat: Chart 실습 코드 추가

--- app/src/pages/Chart/MultitypeChart.jsx
+++ app/src/pages/Chart/MultitypeChart.jsx
@@ -1,33 +1,52 @@
-import { Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import {
+  BarController,
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineController,
+  LineElement,
+  LinearScale,
+  Tooltip,
+} from "chart.js";
 import { Chart } from "react-chartjs-2";
 import { faker } from "@faker-js/faker";
 
-ChartJS.register(Legend, Tooltip);
+ChartJS.register(
+  LinearScale, // 선형 스케일을 정의합니다.
+  CategoryScale, // 범주형 스케일을 정의합니다.
+  BarElement, // 막대 요소를 정의합니다.
+  LineElement, // 데이터 포인트를 연결하는 선을 정의합니다.
+  Legend, // 범례를 제공합니다.
+  Tooltip, // 툴팁을 제공합니다.
+  LineController, // 선 차트를 제어하는 데 사용됩니다.
+  BarController // 막대 차트를 제어하는 데 사용됩니다.
+);
 
-const labels = ["January", "February", "March"];
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
-export const data = {
+const data = {
   labels,
   datasets: [
     {
       type: "line",
-      label: "Data 1",
+      label: "Dataset 1",
       borderColor: "rgb(255, 99, 132)",
       borderWidth: 2,
       fill: false,
       data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
     },
     {
-      type: "bubble",
-      label: "Data 2",
+      type: "bar",
+      label: "Dataset 2",
       backgroundColor: "rgb(75, 192, 192)",
       data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
       borderColor: "white",
       borderWidth: 2,
     },
     {
-      type: "scatter",
-      label: "Data 3",
+      type: "bar",
+      label: "Dataset 3",
       backgroundColor: "rgb(53, 162, 235)",
       data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
     },
