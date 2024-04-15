424d818bc21648311e588ff13a62845b4d7aeb43
wSeungMi <seungmi.dev@gmail.com>
2024-04-15 17:43:47 +0900

    Feat: 이미지 파일 등록 및 미리보기 구현

--- app/src/pages/ColorPicker/index.jsx
+++ app/src/pages/ColorPicker/index.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import S from "./index.module.scss";
 
 const ColorPicker = () => {
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleInputImage = (e) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setPreviewImage(URL.createObjectURL(file));
+    }
+
+    // 메모리 누수 방지를 위해 파일을 선택할 때마다 이전 URL 해제
+    if (previewImage) {
+      URL.revokeObjectURL(previewImage);
+    }
+  };
+
   return (
     <div className={S.wrapper}>
       <h1 className={S.title}>Color Picker!</h1>
@@ -9,7 +24,12 @@ const ColorPicker = () => {
           <div className={S.formSection}>
             <p>1. 이미지를 선택</p>
             <label htmlFor="image"></label>
-            <input type="file" id="image" accept="image/*" />
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              onChange={handleInputImage}
+            />
           </div>
 
           <div className={S.formSection}>
@@ -25,7 +45,16 @@ const ColorPicker = () => {
           </div>
         </div>
 
-        <div className={S.rightColumn}></div>
+        <div className={S.rightColumn}>
+          {previewImage ? (
+            <>
+              <img src={previewImage} alt="image" />
+              <div style={{ backgroundImage: `url(${previewImage})` }}></div>
+            </>
+          ) : (
+            <p>No Image</p>
+          )}
+        </div>
       </div>
     </div>
   );
